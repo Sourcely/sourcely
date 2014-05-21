9bd8050de50c137fbbe04bb90e04365c3f83cc45
brianlk <brian.kang2@gmail.com>
2014-05-21 10:20:39 -0700

    left collapsable menu

--- public/webClient/js/controllers/technologyController.js
+++ public/webClient/js/controllers/technologyController.js
@@ -1,4 +1,4 @@
-app.controller('technology', ['$scope', '$http', 'techFactory', function($scope, $http, techFactory){
+app.controller('technologyController', ['$scope', '$http', 'techFactory', function($scope, $http, techFactory){
   $scope.category = {name:"Technology", articles: techFactory.getTechArticles()};
   if($scope.category.articles === undefined) {
     $http({ method:'GET',
