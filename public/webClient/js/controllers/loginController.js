94edc012ea771cd7d9a8f57b94ea860c6c4d9604
Scott Z <Scottzhang235@gmail.com>
2014-06-02 18:05:10 -0700

    login modal, yay

--- public/webClient/js/controllers/loginController.js
+++ public/webClient/js/controllers/loginController.js
@@ -1,10 +1,11 @@
-app.controller('loginController', ['$scope', '$http', function($scope, $http){
-  $scope.test= function(){
+app.controller('loginController', ['$scope', '$http','$modalInstance', function($scope, $http, $modalInstance){
+  $scope.signup = true;
+/*  $scope.test= function(){
     console.log("from login controller")
     console.log(arguments);
   };
   $scope.login = function (username, password) {
     var user = {username: username, password: password};
     $http.post('/login', user).success(console.log("user ", user.username + " has been logged in"));  
-  };  
+  };  */
 }]);
