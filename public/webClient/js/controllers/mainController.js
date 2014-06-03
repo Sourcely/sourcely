38b3794aa78d1f68054b76e47342cf21cd525462
brianlk <brian.kang2@gmail.com>
2014-06-02 17:30:39 -0700

    login/signup

--- public/webClient/js/controllers/mainController.js
+++ public/webClient/js/controllers/mainController.js
@@ -1,11 +1,23 @@
-app.controller('mainController', ['$scope', '$http', function($scope, $http){
+app.controller('mainController', ['$scope', '$http', '$modal', function($scope, $http, $modal){
 
   $scope.open = false;
+
+  $scope.loggedIn = false;
+
+  $scope.accountName = "";
 
   $scope.collapseLeft = function() {
     app.setContentWidth($scope.open);
     $scope.open = !$scope.open;
     console.log($scope.open);
+  };
+
+  $scope.openModal = function (logOrSign) {
+    console.log(logOrSign);
+    var modalInstance = $modal.open({
+      templateUrl: 'webClient/templates/'+ logOrSign +'.html',
+      size: 'sm'
+    });
   };
 
 }]);
