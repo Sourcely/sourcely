ed50be55395dccece27b0f69f832e606cd293466
Scott Z <Scottzhang235@gmail.com>
2014-07-22 00:05:58 -0700

    refactored

---
+++ public/client/shellView/components/auth/authServices.js
@@ -0,0 +1,18 @@
+angular.module('auth')
+.factory('authInterceptor', function ($rootScope, $q, $window) {
+  return {
+    request: function (config) {
+      config.headers = config.headers || {};
+      if ($window.localStorage.token) {
+        config.headers.Authorization = $window.localStorage.token;
+      }
+      return config;
+    },
+    response: function (response) {
+      if (response.status === 401) {
+        console.log('token deleted');
+      }
+      return response || $q.when(response);
+    }
+  };
+});
---
+++ public/client/shellView/components/storyList/directives/eventIncludeController.js
@@ -0,0 +1,17 @@
+angular.module('webClient').controller('eventInclude', ['$scope', '$http', '$rootScope', function($scope, $http, $rootScope){
+  var iframe = document.getElementById('articleIFrame');
+  $scope.openLink = function (articleUrl, collectionID) {
+    iframe.src = articleUrl;
+    var collection = {clusterId: collectionID, username: $rootScope.accountName};
+    if($rootScope.loggedIn){
+      $http.post('/api/markread', collection)
+      .success(function(){
+        if($rootScope.readArticles.indexOf(collection.clusterId)===-1){
+            $rootScope.readArticles.push(collection.clusterId);
+        }
+      }).error(function(data, status, headers, config) {
+        console.log("error: ", data);
+      });
+    }
+  };
+}]);
